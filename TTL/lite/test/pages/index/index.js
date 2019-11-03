52006c3b2b18b24feddf919dae1460fe7d4fd280
刘桥 <3175621008@qq.com>
2019-11-03 11:20:03 +0800

    add

--- TTL/lite/test/pages/index/index.js
+++ TTL/lite/test/pages/index/index.js
@@ -7,9 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list: '',
-    word: '',
-    message: '',
+    isAdd: '',
     pId:'',
     skuId:''
   },
--- TTL/lite/test/pages/search/search.js
+++ TTL/lite/test/pages/search/search.js
@@ -1,66 +1,49 @@
+//index.js
+//获取应用实例
 const app = getApp()
-Page({
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    
+    word: '',
+    products: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  //获取输入框的内容
+  word_input: function (e) {
+    this.setData({
+      word: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  // houduanButton2的网络请求
+  houduanButton2: function () {
+    var that = this;
+    wx.request({
+      url: 'http://localhost:8080/searchWord',
+      data: {
+        word: that.data.word
+      },
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)//打印到控制台
+        var products = res.data;
+        if (products == null) {
+          var toastText = '数据获取失败';
+          wx.showToast({
+            title: toastText,
+            icon: '',
+            duration: 2000
+          });
+        } else {
+          that.setData({
+            products: products
+          })
+        }
+      }
+    })
   }
 })
